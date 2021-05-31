3e6d8f6dc79b92097ecae6dc2645e5eeed64134a
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-31 14:51:53 +0530

    pushing employee attendance

--- HelloEmp.java
+++ HelloEmp.java
@@ -1,8 +1,16 @@
 package javaEmp;
-
 public class HelloEmp {
 	public static void main(String[] args){
-		System.out.println("welcome to employee wage computation");
+		int is_full_present=1;
+		double random=Math.floor(Math.random()*10)%2;
+		System.out.println("random value is:"+random);
+		if (random==is_full_present) 
+		{
+			System.out.println("employee is present");
+		}
+		else
+		{
+			System.out.println("employee is absent");			
+		}
 	}
-
 }
