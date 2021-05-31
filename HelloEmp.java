e74c2590f9415730895d83927e9bcbed86961859
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-31 18:26:59 +0530

    terminating loop when it meets a certain condition

--- HelloEmp.java
+++ HelloEmp.java
@@ -1,4 +1,5 @@
 package javaEmp;
+import java.util.*;
 public class HelloEmp {
 	public static void main(String[] args){
 		int is_full_present=1; 
@@ -9,20 +10,31 @@ public class HelloEmp {
 		int salary;
 		int working_days=20;
 		int salary_per_month=0;
-		for (int i=1;i<working_days;i++)
-		{
-			double random= Math.floor(Math.random()*10)%2;
-			if (is_full_present==random)
-			{
-				salary=(wage_per_hr*full_day_work_hr);
-				salary_per_month=(salary_per_month+salary);
+		int total_working_hrs=0;
+			//System.out.println("random variable is:"+random);
+		int i;
+		  for (i=0;i<working_days;i++)
+		  {
+				Random var = new Random();
+				int random;
+				random=var.nextInt(3);
+				switch(random) {
+				case 1:
+					salary=(wage_per_hr*full_day_work_hr);
+					salary_per_month=(salary_per_month+salary);
+					total_working_hrs=(total_working_hrs+full_day_work_hr);
+					break;
+				case 2:
+					salary=(wage_per_hr*part_day_work_hr);
+					salary_per_month=(salary_per_month+salary);
+					total_working_hrs=(total_working_hrs+part_day_work_hr);
+					break;
+			  }
+				if (total_working_hrs==100 || i==20)
+				{
+					break;
+				}
 			}
-			else if (is_part_present==random)
-			{
-				salary=(wage_per_hr*part_day_work_hr);
-				salary_per_month=(salary_per_month+salary);
-			}
-		}
-		System.out.println("salary per month is"+salary_per_month);
+		System.out.println("salary is credited for" +total_working_hrs+ "hours"  +i+ "days"+salary_per_month);
 	}
 }
