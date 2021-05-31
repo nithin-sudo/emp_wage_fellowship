26b8e7ba688c8ad66e21a64e8e3923e2683a873e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-31 15:50:56 +0530

    employee wage computation using switch case

--- HelloEmp.java
+++ HelloEmp.java
@@ -1,28 +1,31 @@
 package javaEmp;
+import java.util.*;
 public class HelloEmp {
 	public static void main(String[] args){
-		int is_full_present=1;
-		int is_part_present=2;
+		int is_full_present=1; 
+		int is_part_present=2; 
 		int wage_per_hr=20;
-		int full_day_work_hr=8;
-		int part_day_work_hr=4;
+		int full_day_work_hr=8; 
+		int part_day_work_hr=4; 
 		int salary;
-		double random=Math.floor(Math.random()*10)%3;
-		System.out.println("random value is:"+random);
-		if (random==is_full_present) 
-		{
+		//double random= Math.floor(Math.random()*10)%3;
+		//System.out.println("random value is:"+random);
+		Random var = new Random();
+		int random;
+		random=var.nextInt(3);
+		System.out.println("random variable is:"+random);
+		switch (random) {
+		case 1: 
 			System.out.println("employee is present");
 			salary=(wage_per_hr*full_day_work_hr);
 			System.out.println("salary is:"+salary);
-		}
-		else if (random==is_part_present)
-		{
+			break;
+		case 2:
 			salary=(wage_per_hr*part_day_work_hr);
 			System.out.println("salary is :"+salary);
 			System.out.println("employee is part time present");
-		}
-		else
-		{
+		    break;
+		default:
 			System.out.println("when employee is absent for the day his salary is zero");					
 		}
 	}
