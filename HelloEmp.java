dd2241614965a236ba22ba47f5d706a7d3310521
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-31 15:22:53 +0530

    added employee parttime salary

--- HelloEmp.java
+++ HelloEmp.java
@@ -2,21 +2,28 @@ package javaEmp;
 public class HelloEmp {
 	public static void main(String[] args){
 		int is_full_present=1;
+		int is_part_present=2;
 		int wage_per_hr=20;
 		int full_day_work_hr=8;
+		int part_day_work_hr=4;
 		int salary;
-		double random=Math.floor(Math.random()*10)%2;
+		double random=Math.floor(Math.random()*10)%3;
 		System.out.println("random value is:"+random);
 		if (random==is_full_present) 
 		{
 			System.out.println("employee is present");
 			salary=(wage_per_hr*full_day_work_hr);
-			System.out.println(salary);
+			System.out.println("salary is:"+salary);
+		}
+		else if (random==is_part_present)
+		{
+			salary=(wage_per_hr*part_day_work_hr);
+			System.out.println("salary is :"+salary);
+			System.out.println("employee is part time present");
 		}
 		else
 		{
-			System.out.println("when employee is absent for the day his salary is zero");		
-			System.out.println("employee is absent");			
+			System.out.println("when employee is absent for the day his salary is zero");					
 		}
 	}
 }
